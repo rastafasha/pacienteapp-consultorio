d67bb8da7521ec3169de500526078a5f79876043
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-02-28 15:35:43 -0400

    actualizado routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { useHash: true }),
+    RouterModule.forRoot(routes, { useHash: false }),
     PagesRoutingModule,
     AuthRoutingModule
   ],
