ce95c962d2f8aa936942b07d4684365413873ef5
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-03-02 16:16:34 -0400

    update TypeScript version; modify environment configuration for paciente; adjust output path in angular.json

--- src/environments/environment.paciente.ts
+++ src/environments/environment.paciente.ts
@@ -1,11 +1,7 @@
 export const environment = {
     production: true,
     //local
-    // url_backend: 'http://127.0.0.1:8000/',
-    //   url_servicios: 'http://127.0.0.1:8000/api',
-    //   url_frontend: 'http://localhost:4300/',
-    //   url_media: 'http://127.0.0.1:8000/storage/',
-      
+
       //remoto consultorio
       url_backend: 'https://consultorio.health-connect.me/backend-api-citas/',
       url_servicios: 'https://consultorio.health-connect.me/backend-api-consultorio/public/api',
