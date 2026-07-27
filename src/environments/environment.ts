558d994773d9b40c60392066032b209555a1299b
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-07-27 12:45:48 -0400

    env render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,23 +6,16 @@ export const environment = {
   production: false,
   //local
   url_backend: 'http://127.0.0.1:8000/',
-    url_servicios: 'http://127.0.0.1:8000/api',
-    url_frontend: 'http://localhost:4300/',
-    url_media: 'http://127.0.0.1:8000/storage/',
-    
-    //remoto consultorio
-    // url_backend: 'https://consultorio.klyntic.com/backend-api-citas/',
-    // url_servicios: 'https://consultorio.klyntic.com/backend-api-consultorio/public/api',
-    // url_frontend: 'https://pconsultorio.klyntic.com/',
-    // url_media: 'https://consultorio.klyntic.com/backend-api-consultorio/storage/app/public/',
+  url_servicios: 'http://127.0.0.1:8000/api',
+  url_frontend: 'http://localhost:4300/',
+  url_media: 'http://127.0.0.1:8000/storage/',
+  backend_node: "http://localhost:3000/api",
 
-     backend_node:"http://localhost:3000/api",
-    
-    //conexion a node y manejo de notificaciones
-    // backend_node:"https://back-klyntic-envios.onrender.com/api",
-    urlBackedNotification:'https://back-klyntic-envios.onrender.com/api/notipush/save-subscription',
-    VAPI_KEY_PUBLIC: 'BG-UDqYJkOikTb0G7nNdKcpqZm__XCl0dwbJsx-kerpEecxL5rp079U7UMZxqo5XA0i60NGOVlezm1RAMyHRTbQ',
-  
+  //conexion a node y manejo de notificaciones
+  // backend_node:"https://back-klyntic-envios.onrender.com/api",
+  urlBackedNotification: 'https://back-klyntic-envios.onrender.com/api/notipush/save-subscription',
+  VAPI_KEY_PUBLIC: 'BG-UDqYJkOikTb0G7nNdKcpqZm__XCl0dwbJsx-kerpEecxL5rp079U7UMZxqo5XA0i60NGOVlezm1RAMyHRTbQ',
+
 
 };
 
